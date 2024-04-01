77ea7288b704da1158d0ab18177e2f8ed8ea2fa0
João Oliveira <38820667+jotavare@users.noreply.github.com>
2024-04-01 16:16:50 +0100

    feat(cpp_01): add ex00 to ex06

--- cpp_00/exercises/ex01/includes/Contact.hpp
+++ cpp_00/exercises/ex01/includes/Contact.hpp
@@ -1,17 +1,20 @@
 #ifndef CONTACT_HPP
 #define CONTACT_HPP
 
+// libraries
 #include <cstdlib>  // EXIT_SUCCESS
 #include <iomanip>  // std::setw
 #include <iostream> // std::cout, std::cin, std::endl, std::string
 #include <limits.h> // INT_MAX
 
-#define RED "\e[31m"
-#define GREEN "\e[32m"
-#define RESET "\e[0m"
-#define BOLD "\e[1m"
+// colored output
+#define RED "\e[31m"    // Red color
+#define GREEN "\e[32m"  // Green color
+#define RESET "\e[0m"   // Reset color
+#define BOLD "\e[1m"    // Bold formatting
 
-class Contact {
+class Contact
+{
 public:
   std::string firstName;
   std::string lastName;
