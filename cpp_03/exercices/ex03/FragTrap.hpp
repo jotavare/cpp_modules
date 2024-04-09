d3c890b75042f61e3ab797c5138f8ab361970619
João Oliveira <38820667+jotavare@users.noreply.github.com>
2024-04-09 17:24:33 +0100

    feat(cpp_03): add ex00 to ex03 the ClapTrap hierarchy

---
+++ cpp_03/exercices/ex03/FragTrap.hpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   FragTrap.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jotavare <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/09 17:22:52 by jotavare          #+#    #+#             */
+/*   Updated: 2024/04/09 19:17:02 by jotavare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <iostream>
+#include "ClapTrap.hpp"
+
+class FragTrap : virtual public ClapTrap
+{
+public:
+    FragTrap();
+    FragTrap(std::string name);
+    FragTrap(const FragTrap &copy);
+    FragTrap &operator=(const FragTrap &copy);
+    ~FragTrap(void);
+
+    void highFivesGuys(void);
+};
---
+++ examples/Heritage3.hpp
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Heritage3.hpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jotavare <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/09 15:59:37 by jotavare          #+#    #+#             */
+/*   Updated: 2024/04/09 16:07:13 by jotavare         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+class Leg
+{
+    // add Leg class definition here
+};
+
+class Quadruped
+{
+private:
+    std::string _name;
+
+    // can be acess by inherited classes
+protected:
+public:
+    void run();
+};
